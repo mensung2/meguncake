00081bc0a82d68de32a52f713eff4eaad19e1042
minseo <mensung6@gmail.com>
2024-07-16 23:35:36 +0900

    엔딩 이미지 수정

--- src/components/Ending1.jsx
+++ src/components/Ending1.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import end1 from "../assets/end1.png";
+import styled from "styled-components";
 
 const Ending1 = () => {
   return (
     <>
-      <img src={end1} />
+      <Stimg src={end1} />
     </>
   );
 };
+
+const Stimg = styled.img`
+  width: 1200px;
+  height: 862px;
+`;
 
 export default Ending1;
--- src/components/Ending2.jsx
+++ src/components/Ending2.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import end2 from "../assets/end2.png";
+import styled from "styled-components";
 
 const Ending2 = () => {
   return (
     <>
-      <img src={end2} />
+      <Stimg src={end2} />
     </>
   );
 };
+
+const Stimg = styled.img`
+  width: 1200px;
+  height: 862px;
+`;
 
 export default Ending2;
